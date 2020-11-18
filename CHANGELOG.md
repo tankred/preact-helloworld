eafb1fce6088dc120ddeb0b41e55dd03966a2cbf
Kurt Duyck <kurt.duyck@vives.be>
2020-11-18 16:28:33 +0100

    Switch content sample
    	modified:   CHANGELOG.md
    	modified:   package.json
    	modified:   src/routes/art/index.js

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.1.9] - 2020-11-18
+### Changed
+- Art Component (A/B)
+
 ## [0.1.8] - 2020-11-16
 ### Changed
 - Art Component ()
--- src/routes/art/index.js
+++ src/routes/art/index.js
@@ -1,14 +1,21 @@
 import { h, Component } from 'preact';
 import style from './style';
 
+  let content = 'undefined'
+
 export default class Art extends Component {
 	
 	// Note: `article` comes from the URL, courtesy of our router
 	render({ article }, { }) {
-		return (
+  	  if ({article} = 'Jandl') {
+  	    content = 'Ernst J. '
+  	  } else {
+  	    content = 'default'
+  	  }
+  		return (
 			<div class={style.profile}>
 				<h1>Profile: {article}</h1>
-				<p>ART content goes here.</p>
+				<p>ART { content } content goes here.</p>
 			</div>
 		);
 	}
